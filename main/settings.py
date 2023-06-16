8143f5946b30c8984123af3e6605712ee0906848
Fyodor <mlgswagdab@gmail.com>
2023-06-17 00:21:07 +0300

    Initialzied starting environment

---
+++ main/settings.py
@@ -0,0 +1,11 @@
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+TILESIZE = 64
+
+WORLD_MAP = [
+['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+['tile', ' ', 'player', ' ', ' ', ' ', ' ', 'tile'],
+['tile', ' ', ' ', ' ', ' ', ' ', ' ', 'tile'],
+['tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile', 'tile'],
+]
