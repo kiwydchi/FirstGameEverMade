856886f86daecdef42388ed4544df613e58ec3d8
Fyodor <mlgswagdab@gmail.com>
2023-06-24 15:24:23 +0300

    Partially fixed pngs

--- main/tile.py
+++ main/tile.py
@@ -6,5 +6,6 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
         self.image = pygame.image.load('main/graphics/test/tile.png').convert_alpha()
+        #self.image = pygame.image.load('main/graphics/test/tile.png').set_colorkey(pygame.Color(223, 72, 197))
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -10)
